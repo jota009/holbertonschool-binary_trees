f9222599f82ae940367eb6ccdb08c665dfec0057
josniel <josnielramos@gmail.com>
2025-04-07 21:19:43 -0400

    1-binary_tree_insert_left.c double free error fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,12 +17,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
-
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->left == NULL)
+	if (parent->left != NULL)
+	{
 		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
 
 	parent->left = new_node;
 
